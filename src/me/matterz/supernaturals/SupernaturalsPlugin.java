e6e8dd0fbc249ce7d37f986d153e649585a822ba
Connor <conconand@gmail.com>
2012-03-21 19:03:45 -0400

    Fixed unused imports.

--- src/me/matterz/supernaturals/SupernaturalsPlugin.java
+++ src/me/matterz/supernaturals/SupernaturalsPlugin.java
@@ -73,7 +73,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
